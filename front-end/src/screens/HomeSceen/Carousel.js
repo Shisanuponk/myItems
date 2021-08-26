e1876d1179112b4f524bbebc040025429cee0a9d
Shisanuponk <89283755+Shisanuponk@users.noreply.github.com>
2021-08-26 23:28:34 +0700

    uninstall package
    uninstall bootstrap andbootstarp-carousel

--- front-end/src/screens/HomeSceen/Carousel.js
+++ front-end/src/screens/HomeSceen/Carousel.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
+// import { Carousel } from 'react-elastic-carousel';
 
-const Carousel = () => {
+const CarouSel = () => {
     return (
         <div>
-            
+        {/* //      <Carousel>
+        //         <div>
+        //             <img src="assets/1.jpeg" />
+        //             <p className="legend">Legend 1</p>
+        //         </div>
+        //         <div>
+        //             <img src="assets/2.jpeg" />
+        //             <p className="legend">Legend 2</p>
+        //         </div>
+        //         <div>
+        //             <img src="assets/3.jpeg" />
+        //             <p className="legend">Legend 3</p>
+        //         </div>
+        //     </Carousel> */}
         </div>
     )
 }
 
-export default Carousel
+export default CarouSel
